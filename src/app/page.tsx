ff6c0454468f49817d1c9ed76dd0e837e21a3661
LeanLabiano <leandrodenos@gmail.com>
2024-09-02 19:18:05 -0300

    Fix bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,6 @@ import BotSection from "./components/bot";
 export default function Home() {
   return (
     <main className="h-screen p-3">
-      <div className="h-20 min-w-20 gradient-border-mask  rounded-xl m-5  flex items-center pl-5 ">
-        <p>aplicale los estilos a este</p>
-      </div>
-
       <TopSection />
       <MidSection />
       <BotSection />
